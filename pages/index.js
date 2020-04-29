e61a17b2129e79e5395f318820c7541b30f1e94f
Paco Cu <pacoceur@gmail.com>
2020-04-29 15:37:09 -0500

    trying to fix editor previews

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
-import Hero from '../components/Hero'
-import About from '../components/About'
-import Features from '../components/Features'
-import Portfolio from '../components/Portfolio'
-import Footer from '../components/Footer'
+import Home from '../components/Home'
 import { attributes } from '../content/main.md'
 
 export default () => {
-  const { hero, about, features, portfolio, footer } = attributes;
   return (
-    <div className="container">
-      <Hero data={hero} />
-      <About data={about} />
-      <Features data={features} />
-      <Portfolio data={portfolio} />
-      <Footer data={footer} />
-    </div>
+    <Home attributes={attributes} />
   )
 }
