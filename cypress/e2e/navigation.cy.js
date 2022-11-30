742596ace7449dd5de1851a867c851540702328a
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-11-30 17:26:54 +0100

    Add local-cypress

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,4 +1,4 @@
-import { cy } from 'cypress';
+import { cy } from 'local-cypress';
 
 describe('Can find the different components in the app', () => {
   it('can visit home page and load all page content', () => {
